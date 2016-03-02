dcac85f9c699a010026cd99864dedc0a16f6efc6
hothost87 <hothost.wei@gmail.com>
2016-03-01 20:32:32 -0500

    fse draft

--- jquery-modified.js
+++ jquery-modified.js
@@ -1,9 +1,8 @@
-jQuery.extendBasic = function extendBasic(target, source) {
+function extendBasic(target, source) {
     var name;
     target = target || {};
     for (name in source) {
         target[name] = source[name];
     }
-    
     return target;
 }
